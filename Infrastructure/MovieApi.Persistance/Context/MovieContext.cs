0efbf30779626008e43f2fa0662738fe113dc34d
Emre Baris Erdem <erdem.emre.baris@gmail.com>
2025-02-06 13:52:34 +0300

    Context sınıfı ve yeni entitiyler eklendi. Database bağlantısı kuruldu. Presentation Layer web.api eklendi.

---
+++ Infrastructure/MovieApi.Persistance/Context/MovieContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistance.Context
+{
+	public class MovieContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=MYDRAGON\\MSSQLSERVER1;Initial Catalog=MovieDB;Integrated Security=True;Trust Server Certificate=True");
+		}
+
+		public DbSet<Cast> Casts { get; set; }
+
+		public DbSet<Category> Categories { get; set; }
+
+		public DbSet<Movie> Movies { get; set; }
+
+		public DbSet<Review> Reviews { get; set; }
+
+		public DbSet<Tag> Tags { get; set; }
+
+
+	}
+}
