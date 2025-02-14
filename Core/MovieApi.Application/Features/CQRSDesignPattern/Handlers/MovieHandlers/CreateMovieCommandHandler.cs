16735825eb0f08d83811f0ddecf47d016ed2e5f0
Emre Baris Erdem <erdem.emre.baris@gmail.com>
2025-02-14 13:12:03 +0300

    Ders 8 - CQRS Design Pattern Uygulanması

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/CreateMovieCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class CreateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+		public CreateMovieCommandHandler(MovieContext context)
+		{
+			_context = context;
+		}
+
+		public async void Handle(CreateMovieCommand command) 
+		{
+			await _context.Movies.AddAsync(new Movie
+			{
+				CoverImgUrl = command.CoverImgUrl,
+				CreatedYear = command.CreatedYear,
+				Description = command.Description,
+				Duration = command.Duration,
+				Rating = command.Rating,
+				RealeaseDate = command.RealeaseDate,
+				Status = command.Status,
+				Title = command.Title
+
+			});
+			await _context.SaveChangesAsync();
+		}
+	}
+}
---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+		private readonly MovieContext _context;
+
+		public UpdateMovieCommandHandler(MovieContext context)
+		{
+			_context = context;
+		}
+
+		public async void Handle(UpdateMovieCommand command)
+		{
+			var value = await _context.Movies.FindAsync(command.MovieId);
+
+			value.Rating = command.Rating;
+			value.Status = command.Status;
+			value.Duration = command.Duration;
+			value.CoverImgUrl = command.CoverImgUrl;
+			value.CreatedYear = command.CreatedYear;
+			value.Description = command.Description;
+			value.RealeaseDate = command.RealeaseDate;
+			value.Title = command.Title;
+
+			await _context.SaveChangesAsync();
+			
+		}
+	}
+}
